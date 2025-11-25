811513f1a708199d85c5032d3e101a94107a0e21
wikan1602 <wikanpriambudi@gmail.com>
2025-11-25 16:15:25 +0700

    style: Fix code formatting based on Black/Isort

--- tests/test_infer.py
+++ tests/test_infer.py
@@ -1,9 +1,12 @@
 # tests/test_infer.py
-import pytest
-import numpy as np
 import os
+
+import numpy as np
+import pytest
+
 # Nanti kita akan buat fungsi ini di src
-# from bsort.infer import load_model, preprocess_image 
+# from bsort.infer import load_model, preprocess_image
+
 
 def test_preprocess_image_shape():
     """
@@ -12,10 +15,10 @@ def test_preprocess_image_shape():
     """
     # 1. Buat gambar dummy (Random noise) ukuran sembarang
     dummy_img = np.random.randint(0, 255, (1080, 1920, 3), dtype=np.uint8)
-    
+
     # 2. Panggil fungsi preprocess (Nanti kita buat fungsinya)
     # processed_img = preprocess_image(dummy_img, target_size=224)
-    
+
     # 3. Assert (Cek) apakah outputnya sesuai target
-    # assert processed_img.shape == (1, 3, 224, 224) 
-    assert True # Placeholder biar pass dulu sebelum codenya ada
+    # assert processed_img.shape == (1, 3, 224, 224)
+    assert True  # Placeholder biar pass dulu sebelum codenya ada
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 # tests/test_main.py
 from typer.testing import CliRunner
-from bsort.main import app 
+
+from bsort.main import app
 
 # Kita pakai library 'typer' untuk CLI (akan kita bahas di step selanjutnya)
 runner = CliRunner()
+
 
 def test_app_help():
     """
